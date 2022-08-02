bd33d1fd45c88e0abdd3f6bc601def2e6dbbb04d
ElenaG <freshelf@yandex.ru>
2022-08-03 01:08:10 +0500

    add random

--- Ex004_Div/Program.cs
+++ Ex004_Div/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 10;
-double numberB = 4;
+﻿int numberA = new Random().Next(1, 10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA / numberB);
